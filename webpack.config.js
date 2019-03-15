94590e3809360c14f93b96a04bb5dc2cde82183c
Viviene De Guzman <vivienedeg@gmail.com>
2019-03-15 13:53:14 -0700

    Change file structure and remove bundle.js from gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     bundle: './src/index.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname),
     filename: 'bundle.js'
   },
   module: {
